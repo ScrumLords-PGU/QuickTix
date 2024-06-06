4f03ea8463676b5f31d2b1ccda6e6eb50e0589cc
Alex <tommingoal@gmail.com>
2024-06-06 18:18:10 -0500

    hyperlinked uniqueID so an event can be created by clicking on it.

--- QuickTix/TechnicianForm.Designer.cs
+++ QuickTix/TechnicianForm.Designer.cs
@@ -29,85 +29,43 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
+        private void InitializeComponent(DataGridView dataGridView1)
         {
             components = new System.ComponentModel.Container();
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            button1 = new Button();
-            textBox6 = new TextBox();
-            textBox1 = new TextBox();
             dataGridView1 = new DataGridView();
             bindingSource = new BindingSource(components);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)bindingSource).BeginInit();
             SuspendLayout();
             // 
-            // button1
-            // 
-            button1.Location = new Point(563, 796);
-            button1.Margin = new Padding(2);
-            button1.Name = "button1";
-            button1.Size = new Size(128, 20);
-            button1.TabIndex = 24;
-            button1.Text = "Submit Ticket";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(856, 530);
-            textBox6.Margin = new Padding(2);
-            textBox6.Multiline = true;
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(208, 126);
-            textBox6.TabIndex = 22;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(320, 312);
-            textBox1.Margin = new Padding(2);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(138, 23);
-            textBox1.TabIndex = 25;
+            
             // 
             // dataGridView1
             // 
-            dataGridView1.AccessibleRole = AccessibleRole.ScrollBar;
-            dataGridView1.AllowUserToOrderColumns = true;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.TopLeft;
-            dataGridViewCellStyle1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle1.NullValue = null;
-            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.BackgroundColor = SystemColors.Control;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(62, 23);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridView1.Dock = DockStyle.Top;
+            dataGridView1.Location = new Point(0, 0);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.DefaultCellStyle.Alignment = DataGridViewContentAlignment.TopCenter;
-            dataGridView1.RowTemplate.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridView1.Size = new Size(1857, 346);
-            dataGridView1.TabIndex = 0;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            dataGridView1.Size = new Size(2026, 400);
+            dataGridView1.TabIndex = 25;
             // 
             // TechnicianForm
             // 
             BackColor = Color.WhiteSmoke;
             ClientSize = new Size(2026, 965);
-            Controls.Add(button1);
-            Controls.Add(textBox6);
-            Controls.Add(dataGridView1);
+            
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Name = "TechnicianForm";
             Text = "TechnicianForm";
+            Load += TechnicianForm_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)bindingSource).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private TextBox textBox6;
-        private TextBox textBox1;
        
     }
 }
